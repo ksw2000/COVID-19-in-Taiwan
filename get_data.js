0aa37d09db2fc314c21c893a8e2fabeac683483d
liao2000 <liao2000@livemail.tw>
2021-06-08 21:45:05 +0800

    新增最近14天內數據

--- get_data.js
+++ get_data.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const https = require('https');
 
-
 // get data from CDC
 https.get('https://od.cdc.gov.tw/eic/Day_Confirmation_Age_County_Gender_19CoV.json', (res) => {
     let buffers = [];
@@ -10,31 +9,62 @@ https.get('https://od.cdc.gov.tw/eic/Day_Confirmation_Age_County_Gender_19CoV.js
     });
     res.on('end', () => {
         let jsonData = JSON.parse(buffers);
-        let dateFrom = 20210511;
-        let cityMap = {};
-        jsonData.forEach(element => {
-            if (element['是否為境外移入'] === '否' && Number(element['個案研判日']) >= dateFrom && typeof element['縣市'] != 'undefined') {
-                if (typeof cityMap[element['縣市']] === 'undefined') {
-                    console.log(element['縣市']);
-                    cityMap[element['縣市']] = 1;
-                } else {
-                    cityMap[element['縣市']]++;
+        
+        let before14Day = new Date((new Date()).valueOf() - 1000 * 60 * 60 * 24 * 14);
+            before14Day = before14Day.getFullYear() * 10000 + (before14Day.getMonth() + 1) * 100 + before14Day.getDate();
+
+        let tasks = [{
+            df: 20210511,
+            dataList:[]
+        },{ 
+            df: Number(before14Day),
+            dataList: []
+        }];
+
+
+        for (let i = 0; i < tasks.length; i++){
+            // select data where date > df
+            let cityMap = {};
+            jsonData.forEach(element => {
+                if (element['是否為境外移入'] === '否' && Number(element['個案研判日']) >= tasks[i].df && typeof element['縣市'] != 'undefined') {
+                    if (typeof cityMap[element['縣市']] === 'undefined') {
+                        cityMap[element['縣市']] = 1;
+                    } else {
+                        cityMap[element['縣市']]++;
+                    }
                 }
-            }
-        });
-        console.log(cityMap);
+            });
+
+            // sorting data and get proportion
+            let sum = 0;
+            Object.keys(cityMap).forEach(city => {
+                sum += cityMap[city];
+                tasks[i].dataList.push({
+                    city: city,
+                    num: cityMap[city],
+                    proportion: 0
+                });
+            });
+
+            tasks[i].dataList.sort((a, b) => {
+                return (a.num < b.num) ? 1 : -1;
+            });
+
+            tasks[i].dataList.forEach((e)=>{
+                e.proportion = e.num / sum;
+            });
+        }     
+
         let date = new Date();
-        let Y = date.getFullYear().toString();
-        let M = (date.getMonth() + 1).toString();
-        let D = date.getDate().toString();
         let h = date.getHours().toString();
         let m = date.getMinutes().toString();
-
         let output = {
-            dateFrom: dateFrom,
-            lastModified: `${Y.padStart(4, 0)}/${M.padStart(2, 0)}/${D.padStart(2, 0)}-${h.padStart(2, 0)}:${m.padStart(2, 0)}`,
-            data: cityMap
+            lastModified: `${date.getFullYear() * 10000 + (date.getMonth() + 1) * 100 + date.getDate()}-${h.padStart(2, 0)}:${m.padStart(2, 0)}`,
+            dataList: {
+                from511: tasks[0].dataList,
+                before14: tasks[1].dataList
+            }
         }
-        fs.writeFileSync('city_statistic.json', JSON.stringify(output), { flag: 'w+' })
+        fs.writeFileSync('city_statistic.json', JSON.stringify(output, null, '\t'), { flag: 'w+' })
     });
 });
